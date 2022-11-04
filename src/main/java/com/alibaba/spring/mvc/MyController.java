ef7a4d2ae8cc081dcd80877396a2657b3d0ec1b5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-04 21:44:49 +0300

    @RequestMapping in Controller

--- src/main/java/com/alibaba/spring/mvc/MyController.java
+++ src/main/java/com/alibaba/spring/mvc/MyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -19,22 +20,6 @@ public class MyController {
     public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//
-//        model.addAttribute("description", " - best karatek");
-//
-//        return "show-emp-details-view";
-//    }
 
     @RequestMapping("/showDetails")
     public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
