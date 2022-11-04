aa8864c6d1aaf99eef31aa6811157f6fdade697c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-04 22:05:32 +0300

    change standard value in form

--- src/main/java/com/alibaba/spring/mvc/MyController.java
+++ src/main/java/com/alibaba/spring/mvc/MyController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/employee")
@@ -29,7 +26,12 @@ public class MyController {
 
     @RequestMapping("/showDetails")
     public String showEmployeeDetails(@ModelAttribute("employee") Employee emp) {
-
+        String name = emp.getName();
+        emp.setName("Mr " + name);
+        String surname = emp.getSurname();
+        emp.setSurname(surname + "!");
+        int salary = emp.getSalary();
+        emp.setSalary(salary*100);
         return "show-emp-details-view";
     }
 }
